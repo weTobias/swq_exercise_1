b5c6401d1a1b972999e8f87950ab7c5ae46be605
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-10-26 17:40:48 +0200

    server test finished

--- spec/part03/serverSpec.js
+++ spec/part03/serverSpec.js
@@ -1,12 +1,20 @@
-var request = require('supertest');
-var hello_world_server = require("../../app/hello_world")
+var request = require('request');
+var hello_world_server = "http://localhost:3000/";
 
-// create TEST code here ->
 describe('Server', () => {
-  it('should behave...', (done) => {
-    request(hello_world_server)
-    .get("/")
-    .expect(200)
-    .end((error) => (error) ? done.fail(error) : done());
-  });
+  it('should call rest path ' / ' and return 200', (done) => {
+    request(hello_world_server, function (err, res, body) {
+      expect(200)
+      expect(body).toBe("Hello World")
+      done();
+    })
+  })
+
+  it('should call rest path ' / ' and return 200', (done) => {
+    request(hello_world_server, function (err, res, body) {
+      expect(200)
+      expect(body).not.toBe("This is a failure")
+      done();
+    })
+  })
 });
---
+++ spec/part04/httpSpec.js
@@ -0,0 +1,5 @@
+describe('Name of the group', () => {
+  it('should behave...', () => {
+    //TODO test goes here
+  });
+});
