9793170fcd45df906cbe004ab6b992ba46127aad
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-10-26 22:13:32 +0200

    changed duplicate descriptor

--- spec/part03/serverSpec.js
+++ spec/part03/serverSpec.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var hello_world_server = "http://localhost:3000/";
 
-describe('Server', () => {
+describe('Server test', () => {
   it('should call rest path ' / ' and return 200', (done) => {
     request(hello_world_server, function (err, res, body) {
       expect(200)
@@ -10,7 +10,7 @@ describe('Server', () => {
     })
   })
 
-  it('should call rest path ' / ' and return 200', (done) => {
+  it('should verify false body', (done) => {
     request(hello_world_server, function (err, res, body) {
       expect(200)
       expect(body).not.toBe("This is a failure")
