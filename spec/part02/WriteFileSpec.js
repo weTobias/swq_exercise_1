c3cdf9e15614eb82b1a7deb1070d5d2913eabdeb
Tobias Weigl <weigl@akaryon.com>
2021-10-26 22:15:46 +0200

    moved afterEach

--- spec/part02/WriteFileSpec.js
+++ spec/part02/WriteFileSpec.js
@@ -2,11 +2,11 @@ describe("WriteFileSpec", () => {
     var writer = require("../../lib/writeFile");
     var fs = require('fs');
 
-    afterEach(() => {
-        fs.unlinkSync("./newfile.txt");
-    });
-
     describe("when create file is called", () => {
+        afterEach(() => {
+            fs.unlinkSync("./newfile.txt");
+        });
+        
         it("should create file", () => {
             writer.createFile();
             expect(fs.existsSync("./newfile.txt")).toBe(true);
