59b7dce6ab6a8de13ea204236886b8f84f4a5346
Tobias Weigl <weigl@akaryon.com>
2021-10-26 21:38:00 +0200

    added remote http request spec

--- spec/part04/httpSpec.js
+++ spec/part04/httpSpec.js
@@ -1,5 +1,18 @@
-describe('Name of the group', () => {
-  it('should behave...', () => {
-    //TODO test goes here
+describe('remote HTTP request spec', () => {
+  const https = require('https');
+  it('should respond with a string that contains the word "Bibliothek"', (done) => {
+    const responseData = new Promise((resolve) => {
+      https.get('https://www.fh-joanneum.at/', res => {
+        let data = '';
+        res.on('data', chunk => { data += chunk }) 
+        res.on('end', () => {
+            resolve(data);
+        });
+      });
+    });
+    responseData.then(data => {
+      expect(data).toMatch(/(Bibliothek)/i)
+      done();
+    });
   });
 });
